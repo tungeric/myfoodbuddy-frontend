b6c5af427bc9bab25a16e35be32fa6134c8c9008
Eric Tung <eric.p.tung@gmail.com>
2017-11-12 10:17:45 -0800

    Add styling and routing to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,26 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="splash-ui">
+      <div className="splash-container">
+        <div className="splash-quote">
+          A friend you feed is a friend indeed
+        </div>
+        <div className="splash-ui">
+          <div className="splash-ui-header">
+            What would you like to do?
+          </div>
+          <div className="splash-ui-options">
+            <Link to='/meals' className='splash-ui-button'>
+              <div className="splash-ui-link-text-meals">Track and log meals</div>
+            </Link>
+            <Link to='/data' className='splash-ui-button'>
+              <div className="splash-ui-link-text-data">Analyze your trends</div>
+            </Link>
+            <Link to='/create' className='splash-ui-button'>
+              <div className="splash-ui-link-text-food">Customize food</div>
+            </Link>
+          </div>
+        </div>
       </div>
     )
   }
