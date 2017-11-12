34d909438c7615d67467f81e2fb3bc1fba277603
Eric Tung <eric.p.tung@gmail.com>
2017-11-11 16:11:46 -0800

    Add routing functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Header from './Header'
 import MealIndex from './Meals/MealIndex'
 import CreateFood from './Foods/CreateFood'
+
 
 class App extends Component {
   render() {
     return (
-      <CreateFood />
+      <div className='center w85'>
+        <Header />
+        <div className='ph3 pv1 background-gray'>
+          <Switch>
+            <Route exact path='/' component={MealIndex} />
+            <Route exact path='/create' component={CreateFood} />
+          </Switch>
+        </div>
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+
 // 1
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
 
 // 2
 const httpLink = new HttpLink({ uri: 'https://myfoodbuddy-backend.herokuapp.com/' })
@@ -20,9 +22,11 @@ const client = new ApolloClient({
 
 // 4
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
   , document.getElementById('root')
 )
 registerServiceWorker()
