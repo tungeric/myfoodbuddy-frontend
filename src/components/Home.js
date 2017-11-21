a298e4d48fbe19c365360b91ba677cc1c0f09fe3
Eric Tung <eric.p.tung@gmail.com>
2017-11-21 10:44:49 -0800

    Fix home page link to trends page

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,11 +18,9 @@ class Home extends Component {
             <Link to='/meals' className='splash-ui-button'>
               <div className="splash-ui-link-text-meals">Track and log meals</div>
             </Link>
-            <div to='/data' className='splash-ui-button'>
-              <Link to='/trends' className='splash-ui-button'>
-                <div className="splash-ui-link-text-data">Analyze your trends=</div>
-              </Link>
-            </div>
+            <Link to='/trends' className='splash-ui-button'>
+              <div className="splash-ui-link-text-trends">Analyze your trends=</div>
+            </Link>
             <Link to='/create' className='splash-ui-button'>
               <div className="splash-ui-link-text-food">Customize food</div>
             </Link>
